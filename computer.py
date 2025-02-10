7b3abd3fa5f1d8a2614f427c7282b966750c8a71
GithinjiTabz1 <tgithinji@smith.edu>
2025-02-09 21:04:31 -0500

    Added attributes and methods

--- computer.py
+++ computer.py
@@ -1,10 +1,34 @@
 class Computer:
 
-    # What attributes will it need?
+ # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
+
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self,
+                    des: str,
+                    pro_typ: str,
+                    hdd_cap: int,
+                    mem: int,
+                    os: str,
+                    year: int,
+                    amt: int):
+        
+        self.description = des
+        self.processor_typ = pro_typ
+        self.hard_drive_capacity = hdd_cap
+        self.memory = mem
+        self.operating_system = os
+        self.year_made = year
+        self.price = amt
+     # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+   
--- reflection.md
+++ reflection.md
@@ -1,3 +1,13 @@
 Use this file to record your reflection on this assignment. 
 
 What worked, what didn't, what advice would you give someone taking this course in the future?
+
+What worked
+Relating the tasks  to be done to the objects rather than a method to follow.
+Going to T.A and office hours to ask for help.
+
+What didn't work
+Looking at the problem for a long time without taking a break or asking for help.
+
+What advice I'd give
+Ask for help or take a break whenever you get stuck.
